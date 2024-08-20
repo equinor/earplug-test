e38081b2d413839579bb4831d01c29be303fa13c
Dan André Svendsen <dsven@equinor.com>
2024-08-20 15:30:20 +0200

    refactor: create `getFlooredResult`

--- contexts/_internal/useFlooredDecibelDifferenceResult.ts
+++ contexts/_internal/useFlooredDecibelDifferenceResult.ts
@@ -1,21 +1,17 @@
 import { useMemo } from "react";
 import { DecibelDifferenceResult } from "./types";
-import { createValue, isValue, unwrap } from "../../utils/valueOrError";
+import { getFlooredResult } from "./getFlooredResult";
 
 export const useFlooredDecibelDifferenceResult = (
   decibelDifferenceResult: DecibelDifferenceResult,
 ) => {
-  const flooredDecibelDifferenceResult = useMemo(() => {
-    let left = decibelDifferenceResult.left;
-    let right = decibelDifferenceResult.right;
-    if (isValue(left)) {
-      left = createValue(Math.floor(unwrap(left)));
-    }
-    if (isValue(right)) {
-      right = createValue(Math.floor(unwrap(right)));
-    }
-    return { left, right };
-  }, [decibelDifferenceResult]);
+  const flooredDecibelDifferenceResult = useMemo(
+    () => ({
+      left: getFlooredResult(decibelDifferenceResult.left),
+      right: getFlooredResult(decibelDifferenceResult.right),
+    }),
+    [decibelDifferenceResult],
+  );
 
   return { flooredDecibelDifferenceResult };
 };
